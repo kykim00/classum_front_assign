abc3ca0f5682da93530bcd25c4a011d7a08f0b51
kykim00 <jeongsaeki@likelion.org>
2022-03-30 18:50:02 +0900

    🍱 icon svg 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as AddIcon } from "./icon/add.svg";
+export { default as CopyIcon } from "./icon/copy.svg";
+export { default as TrashIcon } from "./icon/trash.svg";
+export { default as PreviewIcon } from "./icon/preview.svg";
