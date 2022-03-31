9eb3af8b5942417910187445e41fd20f910d6175
kykim00 <jeongsaeki@likelion.org>
2022-04-01 08:35:42 +0900

    ✨ 체크박스 미리보기 구현

---
+++ src/components/preview/CheckAnswer.tsx
@@ -0,0 +1,43 @@
+import { Checkbox } from "@mui/material";
+import { useDispatch } from "react-redux";
+import {
+  Question,
+  removeAnswer,
+  setMultipleAnswer,
+} from "../../stores/question";
+import { CardContainer } from "../common/layouts/CardContainer";
+import { FlexBox } from "../common/layouts/FlexBox";
+import { QuestionTitle, RedPoint } from "../common/style";
+
+export const CheckAnswer = ({ id, title, isEssential, options }: Question) => {
+  const dispatch = useDispatch();
+
+  const onChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked)
+      dispatch(
+        setMultipleAnswer({
+          id: id,
+          answerId: +e.target.name.slice(-1),
+          answer: e.target.value,
+        })
+      );
+    else dispatch(removeAnswer({ id: id, answerId: +e.target.name.slice(-1) }));
+  };
+  return (
+    <CardContainer>
+      <QuestionTitle>
+        {title} {isEssential && <RedPoint>*</RedPoint>}
+      </QuestionTitle>
+      {options.map((option, index) => (
+        <FlexBox key={`${index}_${id}`} justifyContent="flex-start">
+          <Checkbox
+            onChange={onChangeAnswer}
+            value={option.option}
+            name={`OPTION${option.id}`}
+          />
+          <label>{option.option}</label>
+        </FlexBox>
+      ))}
+    </CardContainer>
+  );
+};
