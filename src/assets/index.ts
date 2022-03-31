7c537f30d68834213c04d92a6175b228334580cc
kykim00 <jeongsaeki@likelion.org>
2022-03-31 10:39:12 +0900

    🍱 remove icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as AddIcon } from "./icon/add.svg";
 export { default as CopyIcon } from "./icon/copy.svg";
 export { default as TrashIcon } from "./icon/trash.svg";
 export { default as PreviewIcon } from "./icon/preview.svg";
+export { default as RemoveIcon } from "./icon/remove.svg";
