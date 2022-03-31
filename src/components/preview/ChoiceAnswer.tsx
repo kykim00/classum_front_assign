38e195712983bc06421240da781cddcc606c8090
kykim00 <jeongsaeki@likelion.org>
2022-04-01 08:35:14 +0900

    ✨ 객관식 미리보기 구현

---
+++ src/components/preview/ChoiceAnswer.tsx
@@ -0,0 +1,41 @@
+import { Radio } from "@mui/material";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Question, setSingleAnswer } from "../../stores/question";
+import { CardContainer } from "../common/layouts/CardContainer";
+import { FlexBox } from "../common/layouts/FlexBox";
+import { QuestionTitle, RedPoint } from "../common/style";
+
+export const ChoiceAnswer = ({ id, title, isEssential, options }: Question) => {
+  const [selectedValue, setSelectedValue] = useState("");
+  const dispatch = useDispatch();
+
+  const onChangeAnswer = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedValue(e.target.value);
+    dispatch(
+      setSingleAnswer({
+        id: id,
+        answerId: 1,
+        answer: e.target.value,
+      })
+    );
+  };
+  return (
+    <CardContainer>
+      <QuestionTitle>
+        {title} {isEssential && <RedPoint>*</RedPoint>}
+      </QuestionTitle>
+      {options.map((option, index) => (
+        <FlexBox key={`${index}_${id}`} justifyContent="flex-start">
+          <Radio
+            checked={selectedValue === option.option}
+            onChange={onChangeAnswer}
+            value={option.option}
+            name={`OPTION${option.id}`}
+          />
+          <label>{option.option}</label>
+        </FlexBox>
+      ))}
+    </CardContainer>
+  );
+};
